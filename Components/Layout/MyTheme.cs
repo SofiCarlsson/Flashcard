663458b636a56c1743065c67bd9e298eb391c1a7
Sofi <carlssonsofi99@gmail.com>
2026-06-18 10:36:59 +0200

    Eget tema för MudBlazor komponenter

---
+++ Components/Layout/MyTheme.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace Flashcard.Components.Layout
+{
+    public class MyTheme : MudTheme
+    {
+        public MyTheme() 
+        { 
+            PaletteLight = new PaletteLight()
+            {
+                Primary = "#CB7000", // Första knappen
+                Secondary = "#FFCAA0", // Andra knappen
+                Background = "#fdf6e3", // Body
+                Surface = "#FFFFFF", // Paper, kort osv.
+                TextPrimary = "#140600",
+                TextSecondary = "#3E1E00",
+
+                AppbarBackground = "#FF8F00",
+
+                DrawerBackground = "#FFCAA0",
+                DrawerText = "#191B1C"
+            };
+        }
+    }
+}
