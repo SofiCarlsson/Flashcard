c342ea7392ad97c4f4cad06848ec38128327adb4
Sofi <carlssonsofi99@gmail.com>
2026-06-25 21:42:49 +0200

    Första databasen med seed data för HskLevel och Word

--- Data/FlashcardDbContext.cs
+++ Data/FlashcardDbContext.cs
@@ -13,9 +13,56 @@ namespace Flashcard.Data
 
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            // Lägg till Seeddata i tabellerna
+            SeedDataHskLevels(modelbuilder);
+            SeedDataWords(modelbuilder);
+        }
+
+        private static void SeedDataHskLevels(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HskLevel>().HasData(
+                new HskLevel { HskLevelId = 1, Name = "HSK 1" },
+                new HskLevel { HskLevelId = 2, Name = "HSK 2" },
+                new HskLevel { HskLevelId = 3, Name = "HSK 3" });
+        }
+
+        private static void SeedDataWords(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Word>().HasData(
+                new Word { WordId = 1, Character = "爱", Pinyin = "ài", Meaning = "to love", HskLevelId = 1 },
+                new Word { WordId = 2, Character = "八", Pinyin = "bā", Meaning = "eight", HskLevelId = 1 },
+                new Word { WordId = 3, Character = "爸爸", Pinyin = "bàba", Meaning = "father", HskLevelId = 1 },
+                new Word { WordId = 4, Character = "杯子", Pinyin = "bēizi", Meaning = "cup; glass; mug", HskLevelId = 1 },
+                new Word { WordId = 5, Character = "北京", Pinyin = "běijīng", Meaning = "Beijing", HskLevelId = 1 },
+                new Word { WordId = 6, Character = "本", Pinyin = "běn", Meaning = "measure word for books", HskLevelId = 1 },
+                new Word { WordId = 7, Character = "不客气", Pinyin = "búkèqi", Meaning = "you're welcome", HskLevelId = 1 },
+                new Word { WordId = 8, Character = "不", Pinyin = "bù", Meaning = "not; no", HskLevelId = 1 },
+                new Word { WordId = 9, Character = "菜", Pinyin = "cài", Meaning = "vegetable; dish", HskLevelId = 1 },
+                new Word { WordId = 10, Character = "茶", Pinyin = "chá", Meaning = "tea", HskLevelId = 1 },
+                new Word { WordId = 11, Character = "吧", Pinyin = "ba", Meaning = "[particle making suggestion, e.g. 'let’s…']", HskLevelId = 2 },
+                new Word { WordId = 12, Character = "白", Pinyin = "bái", Meaning = "white", HskLevelId = 2 },
+                new Word { WordId = 13, Character = "百", Pinyin = "bǎi", Meaning = "hundred", HskLevelId = 2 },
+                new Word { WordId = 14, Character = "帮助", Pinyin = "bāngzhù", Meaning = "to help", HskLevelId = 2 },
+                new Word { WordId = 15, Character = "报纸", Pinyin = "bàozhǐ", Meaning = "newspaper", HskLevelId = 2 },
+                new Word { WordId = 16, Character = "比", Pinyin = "bǐ", Meaning = "to compare", HskLevelId = 2 },
+                new Word { WordId = 17, Character = "别", Pinyin = "bié", Meaning = "don’t; other; separate", HskLevelId = 2 },
+                new Word { WordId = 18, Character = "宾馆", Pinyin = "bīnguǎn", Meaning = "guesthouse; hotel", HskLevelId = 2 },
+                new Word { WordId = 19, Character = "长", Pinyin = "cháng", Meaning = "long", HskLevelId = 2 },
+                new Word { WordId = 20, Character = "唱歌", Pinyin = "chànggē", Meaning = "to sing; singing", HskLevelId = 2 },
+                new Word { WordId = 21, Character = "阿姨", Pinyin = "āyí", Meaning = "auntie [mother's younger sister]", HskLevelId = 3 },
+                new Word { WordId = 22, Character = "啊", Pinyin = "a", Meaning = "ah", HskLevelId = 3 },
+                new Word { WordId = 23, Character = "矮", Pinyin = "ǎi", Meaning = "short (height)", HskLevelId = 3 },
+                new Word { WordId = 24, Character = "爱好", Pinyin = "àihào", Meaning = "interests; hobbies", HskLevelId = 3 },
+                new Word { WordId = 25, Character = "安静", Pinyin = "ānjìng", Meaning = "to be quiet", HskLevelId = 3 },
+                new Word { WordId = 26, Character = "把", Pinyin = "bǎ", Meaning = "grasp; measure word for knives", HskLevelId = 3 },
+                new Word { WordId = 27, Character = "搬", Pinyin = "bān", Meaning = "to move", HskLevelId = 3 },
+                new Word { WordId = 28, Character = "班", Pinyin = "bān", Meaning = "class", HskLevelId = 3 },
+                new Word { WordId = 29, Character = "半", Pinyin = "bàn", Meaning = "half", HskLevelId = 3 },
+                new Word { WordId = 30, Character = "办法", Pinyin = "bànfǎ", Meaning = "way; method; solution", HskLevelId = 3 });
+        }
     }
 }
---
+++ Migrations/20260625193942_FirstSeedData.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Flashcard.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "HskLevels",
+                columns: new[] { "HskLevelId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "HSK 1" },
+                    { 2, "HSK 2" },
+                    { 3, "HSK 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Words",
+                columns: new[] { "WordId", "Character", "HskLevelId", "Meaning", "Pinyin" },
+                values: new object[,]
+                {
+                    { 1, "爱", 1, "to love", "ài" },
+                    { 2, "八", 1, "eight", "bā" },
+                    { 3, "爸爸", 1, "father", "bàba" },
+                    { 4, "杯子", 1, "cup; glass; mug", "bēizi" },
+                    { 5, "北京", 1, "Beijing", "běijīng" },
+                    { 6, "本", 1, "measure word for books", "běn" },
+                    { 7, "不客气", 1, "you're welcome", "búkèqi" },
+                    { 8, "不", 1, "not; no", "bù" },
+                    { 9, "菜", 1, "vegetable; dish", "cài" },
+                    { 10, "茶", 1, "tea", "chá" },
+                    { 11, "吧", 2, "[particle making suggestion, e.g. 'let’s…']", "ba" },
+                    { 12, "白", 2, "white", "bái" },
+                    { 13, "百", 2, "hundred", "bǎi" },
+                    { 14, "帮助", 2, "to help", "bāngzhù" },
+                    { 15, "报纸", 2, "newspaper", "bàozhǐ" },
+                    { 16, "比", 2, "to compare", "bǐ" },
+                    { 17, "别", 2, "don’t; other; separate", "bié" },
+                    { 18, "宾馆", 2, "guesthouse; hotel", "bīnguǎn" },
+                    { 19, "长", 2, "long", "cháng" },
+                    { 20, "唱歌", 2, "to sing; singing", "chànggē" },
+                    { 21, "阿姨", 3, "auntie [mother's younger sister]", "āyí" },
+                    { 22, "啊", 3, "ah", "a" },
+                    { 23, "矮", 3, "short (height)", "ǎi" },
+                    { 24, "爱好", 3, "interests; hobbies", "àihào" },
+                    { 25, "安静", 3, "to be quiet", "ānjìng" },
+                    { 26, "把", 3, "grasp; measure word for knives", "bǎ" },
+                    { 27, "搬", 3, "to move", "bān" },
+                    { 28, "班", 3, "class", "bān" },
+                    { 29, "半", 3, "half", "bàn" },
+                    { 30, "办法", 3, "way; method; solution", "bànfǎ" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordId",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "HskLevels",
+                keyColumn: "HskLevelId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "HskLevels",
+                keyColumn: "HskLevelId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "HskLevels",
+                keyColumn: "HskLevelId",
+                keyValue: 3);
+        }
+    }
+}
